cd2216a12dfab2aa1fc48413262046392d23eecc
caroso1222 <ce.roso398@gmail.com>
2016-02-23 01:48:33 -0500

    Added new videos and modules in cotizador

--- public/js/cotizacion.js
+++ public/js/cotizacion.js
@@ -74,18 +74,23 @@ angular.module("quote-app",[])
   "DERMATUDE": "Dermatude",
   "E_LASE": "E-Lase con motif",
   "IPL": "IPL - M22",
-  "LASER_CO2": "Láser CO2"
+  "LASER_CO2": "Láser CO2",
+  "BOTOX": "Botox",
+  "ACIDO": "Ácido Hialurónico",
+  "PLASMA": "Aplicación con plasma"
 })
 .constant("SESSIONS",{
   "SESIONES_3": "3",
   "SESIONES_8": "8",
   "SESIONES_1_2": "Entre 1 y 2",
-  "SESIONES_6_8": "Entre 6 y 8"
+  "SESIONES_6_8": "Entre 6 y 8",
+  "DOS_VECES_ANHO": "2 veces al año",
+  "SEGUN_PIEL": "Según tipo piel"
 })
 .constant("FREQUENCY",{
   "SEMANAS_4": "Cada 4 semanas",
   "SEMANAS_6": "Cada 6 semanas",
-  "DESPUES_UNA_SEMANA": "Después de una semana de la primera sesión se realiza una cita de control y se evalua la neCesidad de una segunda sesión.",
+  "DESPUES_UNA_SEMANA": "Después de una semana de la primera sesión se realiza una cita de control y se evalua la necesidad de una segunda sesión.",
   "PRIMERAS_4": "Las primeras 4 una cada semana, las siguientes una cada 15 días.",
   "SE_RECOMIENDA": "Se recomienda una cada mes dependiendo la evolución."
 })
@@ -264,13 +269,30 @@ angular.module("quote-app",[])
       theRecomm = [RECOMMENDATIONS.E_LASE, SESSIONS.SESIONES_6_8, FREQUENCY.SEMANAS_6];
       this.recommendationsActivated.push(theRecomm); 
     }else if (zone == ZONES.ANTIENVEJECIMIENTO || zone == ZONES.MEJORAR_TEXTURA || zone == ZONES.FLACIDES || zone == ZONES.CELULITIS){
+      if(zone == ZONES.MEJORAR_TEXTURA && this.currentBodyPart == BODY_PARTS.ROSTRO){
+        theRecomm = [RECOMMENDATIONS.ACIDO, SESSIONS.DOS_VECES_ANHO, FREQUENCY.DESPUES_UNA_SEMANA];
+        this.recommendationsActivated.push(theRecomm);
+      }
+
+      if((zone == ZONES.FLACIDES || zone == ZONES.MEJORAR_TEXTURA) && this.currentBodyPart == BODY_PARTS.CUELLO){
+        theRecomm = [RECOMMENDATIONS.BOTOX, SESSIONS.DOS_VECES_ANHO, FREQUENCY.DESPUES_UNA_SEMANA];
+        this.recommendationsActivated.push(theRecomm);
+      }
       theRecomm = [RECOMMENDATIONS.DEEP_3, SESSIONS.SESIONES_8, FREQUENCY.PRIMERAS_4];
       this.recommendationsActivated.push(theRecomm);
     }else if(zone == ZONES.POROS || zone == ZONES.PATAS_GALLO || zone == ZONES.OJERAS || zone == ZONES.LINEAS_EXPRESION 
       || zone == ZONES.BRILLO_PIEL || zone == ZONES.ARRUGAS_FRENTE || zone == ZONES.ARRUGAS_BOCA){
       theRecomm = [RECOMMENDATIONS.DERMATUDE, SESSIONS.SESIONES_3, FREQUENCY.SE_RECOMIENDA];
       this.recommendationsActivated.push(theRecomm);
+      if((zone == ZONES.ARRUGAS_BOCA || ZONES.ARRUGAS_FRENTE || ZONES.LINEAS_EXPRESION || ZONES.PATAS_GALLO) && this.currentBodyPart == BODY_PARTS.ROSTRO){
+        theRecomm = [RECOMMENDATIONS.ACIDO, SESSIONS.DOS_VECES_ANHO, FREQUENCY.DESPUES_UNA_SEMANA];
+        this.recommendationsActivated.push(theRecomm);
+      }
     }else if(zone == ZONES.CICATRICES){
+      if(this.currentBodyPart == BODY_PARTS.ROSTRO || this.currentBodyPart == BODY_PARTS.CUELLO){
+        theRecomm = [RECOMMENDATIONS.PLASMA, SESSIONS.SEGUN_PIEL, FREQUENCY.DESPUES_UNA_SEMANA];
+        this.recommendationsActivated.push(theRecomm);
+      }
       if(this.currentBodyPart == BODY_PARTS.ABDOMEN || this.currentBodyPart == BODY_PARTS.BRAZOS || this.currentBodyPart == BODY_PARTS.GLUTEOS 
         || this.currentBodyPart == BODY_PARTS.MANOS || this.currentBodyPart == BODY_PARTS.MUSLOS || this.currentBodyPart == BODY_PARTS.ROSTRO)
       {
@@ -290,10 +312,22 @@ angular.module("quote-app",[])
     }else if(zone == ZONES.HIPERPIGMENTACION || zone == ZONES.HIPOPIGMENTACION || zone == ZONES.ESTRIAS || zone == ZONES.MELASMAS){
       theRecomm = [RECOMMENDATIONS.IPL, SESSIONS.SESIONES_1_2, FREQUENCY.DESPUES_UNA_SEMANA];
       this.recommendationsActivated.push(theRecomm);
+      if(zone == ZONES.HIPERPIGMENTACION){
+        theRecomm = [RECOMMENDATIONS.PLASMA, SESSIONS.SEGUN_PIEL, FREQUENCY.DESPUES_UNA_SEMANA];
+        this.recommendationsActivated.push(theRecomm);
+      }
     }else if(zone == ZONES.POST_CIRUGIAS || zone == ZONES.SECUELAS_ACNE){
       theRecomm = [RECOMMENDATIONS.LASER_CO2, SESSIONS.SESIONES_1_2, FREQUENCY.DESPUES_UNA_SEMANA];
       this.recommendationsActivated.push(theRecomm);
     }else if(zone == ZONES.ARRUGAS){
+      if(this.currentBodyPart == BODY_PARTS.ROSTRO || this.currentBodyPart == BODY_PARTS.CUELLO){
+        theRecomm = [RECOMMENDATIONS.BOTOX, SESSIONS.DOS_VECES_ANHO, FREQUENCY.DESPUES_UNA_SEMANA];
+        this.recommendationsActivated.push(theRecomm);
+      }
+      if(this.currentBodyPart == BODY_PARTS.ROSTRO){
+        theRecomm = [RECOMMENDATIONS.ACIDO, SESSIONS.DOS_VECES_ANHO, FREQUENCY.DESPUES_UNA_SEMANA];
+        this.recommendationsActivated.push(theRecomm);
+      }
       if(this.currentBodyPart == BODY_PARTS.ROSTRO || this.currentBodyPart == BODY_PARTS.CUELLO || this.currentBodyPart == BODY_PARTS.MANOS){
         theRecomm = [RECOMMENDATIONS.LASER_CO2, SESSIONS.SESIONES_1_2, FREQUENCY.DESPUES_UNA_SEMANA];
         this.recommendationsActivated.push(theRecomm);
